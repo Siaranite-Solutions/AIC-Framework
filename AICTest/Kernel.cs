18a35958ff9a71b8cca78eb8d906b73f646638ca
Arawn Davies <arawn.davies780@gmail.com>
2017-10-12 21:51:58 +0100

    Added demo for menu - will write up in documentation

--- AICTest/Kernel.cs
+++ AICTest/Kernel.cs
@@ -5,6 +5,7 @@ using Sys = Cosmos.System;
 using AIC_Framework;
 using AIC_Framework.Extensions;
 using Console = AIC_Framework.AConsole;
+using menu = AIC_Framework.AConsole.Menu;
 
 namespace AICTest
 {
@@ -19,10 +20,41 @@ namespace AICTest
         
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            menu.Reset();
+            // Create a new category: catTest
+            menu.Category catTest = new menu.Category("Test");
+            catTest.AddEntry(new TestCommand());
+
+            // Create a new category: catHello
+            menu.Category catHello = new menu.Category("Hello");
+            catHello.AddEntry(new HelloWorld());
+
+            // Add the categories to the menu
+            menu.AddCategory(catTest);
+            menu.AddCategory(catHello);
+
+            // Show the menu
+            menu.Show();
+        }
+    }
+    public class TestCommand : menu.Entry
+    {
+        public TestCommand() { this.text = "Test Command"; }
+        public override void Execute()
+        {
+            // Place the code that should be executed here
+        }
+    }
+
+    // A hello world entry
+    public class HelloWorld : menu.Entry
+    {
+        public HelloWorld() { this.text = "Hello World!"; }
+        public override void Execute()
+        {
+            Console.Clear();
+            AConsole.Write("");
+            // Place the code that should be executed here
         }
     }
 }
