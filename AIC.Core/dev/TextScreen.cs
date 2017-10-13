ef1881e83dc3d1c6440b73d059ab4e612764f672
Arawn Davies <arawn.davies780@gmail.com>
2017-10-13 14:32:45 +0100

    Made license notice smaller on each page, redirecting to the Licenses folder

--- AIC.Core/dev/TextScreen.cs
+++ AIC.Core/dev/TextScreen.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+Copyright (c) 2012-2013, dewitcher Team
+Copyright (c) 2017, Apollo OS
+Copyright (c) 2017, Cosmos
+
+All rights reserved.
+
+See in the /Licenses folder for the licenses for each respected project.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
