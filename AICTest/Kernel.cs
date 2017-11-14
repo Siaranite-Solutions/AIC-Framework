654eeabe06778e3dcc7073a2a7fead53e68fedf4
Arawn Davies <arawn.davies780@gmail.com>
2017-11-14 09:44:51 +0000

    Updates

--- AICTest/Kernel.cs
+++ AICTest/Kernel.cs
@@ -9,55 +9,7 @@ using menu = AIC_Framework.AConsole.Menu;
 
 namespace AICTest
 {
-    public class ShutdownEntry: menu.Entry
-    {
-        public ShutdownEntry() { this.text = "Shutdown"; }
-        public override void Execute()
-        {
-            Console.Clear();
-            Console.WriteLine("Press any key to shutdown...");
-            Console.ReadKey(true);
-            userACPI.Shutdown();
-        }
-    }
-    public class RebootEntry : menu.Entry
-    {
-        public RebootEntry() { this.text = "Reboot"; }
-        public override void Execute()
-        {
-            Console.Clear();
-            Console.WriteLine("Press any key to reboot...");
-            Console.ReadKey(true);
-            userACPI.Reboot();
-        }
-    }
-    public class Entry1 : menu.Entry
-    {
-        public Entry1() { this.text = "Entry 1"; }
-        public override void Execute()
-        {
-            Console.Clear();
-            Console.WriteLine("Apollo Test Entry");
-            Console.ReadKey(true);
-            Console.Clear();
-            // Place the code that should be executed here
-        }
-    }
-
-    // A hello world entry
-    public class Entry2 : menu.Entry
-    {
-        public Entry2() { this.text = "Hello World!"; }
-        public override void Execute()
-        {
-            Console.Clear();
-            Console.Write("Hello World!");
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey(true);
-
-            // Place the code that should be executed here
-        }
-    }
+    
     public class Kernel: Sys.Kernel
     {
         protected override void BeforeRun()
@@ -87,6 +39,7 @@ namespace AICTest
 
             // Show the menu
             menu.Show();
+            
         }
     }
 
